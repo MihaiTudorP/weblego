e54f3ed4155b95f694d6a21591a8b712f36e09bd
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-05-03 14:02:49 +0300

    Implemented list display

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,15 @@
-import { User, UserProps } from './models/User';
-import { UserEdit } from './views/UserEdit';
 import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
 import { UserList } from './views/UserList';
 
-const user = User.buildUser({ name: 'Name', age: 20 });
-const domWrapper = document.getElementById('root');
-if (domWrapper)
-    domWrapper.innerHTML = `
-    <div id = "formWrapper"></div>
-    <div id="collectionWrapper"></div>`;
-const container = document.getElementById('formWrapper');
-if (!container) throw new Error('Unable to render form');
-const userEdit = new UserEdit(container, user);
-userEdit.render();
+const users = new Collection('http://localhost:3000/users', (json:UserProps) => {
+    return User.buildUser(json);
+})
 
-const users = new Collection('http://localhost:3000/users', (json: UserProps) => User.buildUser(json));
-
-users.on('change', () => {
-    const wrapper = document.getElementById('collectionWrapper');
-    if (wrapper) {
-        const list = new UserList(wrapper, users);
-        list.render();
+users.on('change', ()=>{
+    const root = document.getElementById('root');
+    if(root){
+        new UserList(root, users).render();
     }
-    else throw new Error('Please create parent div');
-});
-
+})
 users.fetch();
--- src/views/CollectionView.ts
+++ src/views/CollectionView.ts
@@ -3,14 +3,14 @@ import { Collection } from '../models/Collection';
 export abstract class CollectionView<T, K>{
     constructor(public parent: Element, public collection: Collection<T,K>) {}
 
-    render = ():void => {
+    render():void {
         this.parent.innerHTML =  '';
         const templateElement = document.createElement('template');
-        this.collection.models.forEach((model) => {
-            const wrapper = document.createElement('div');
-            this.renderItem(model, wrapper);
-            templateElement.content.append(wrapper);
-        });
+        for(let model of this.collection.models){
+            const itemParent = document.createElement('div');
+            this.renderItem(model, itemParent);
+            templateElement.content.append(itemParent);
+        }
 
         this.parent.append(templateElement.content);
     };
